8a54c5f91a9ed57d94f9fbe0d043f8f3da60e271
Garth <garthd@gmail.com>
2022-01-04 20:41:23 +1300

    DiaryTypeClient

---
+++ VBMS/src/Client/Pages/vbms/diary/diaryType/AddEditDiaryTypeModal.razor.cs
@@ -0,0 +1,62 @@
+﻿using Blazored.FluentValidation;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using MudBlazor;
+using System.Threading.Tasks;
+using VBMS.Application.Features.vbms.diary.diaryType.Commands.AddEdit;
+using VBMS.Client.Extensions;
+using VBMS.Client.Infrastructure.Managers.vbms.diary.diaryType;
+using VBMS.Shared.Constants.Application;
+
+namespace VBMS.Client.Pages.vbms.diary.diaryType
+{
+    public partial class AddEditDiaryTypeModal
+    {
+        [Inject] private IDiaryTypeManager DiaryTypeManager { get; set; }
+
+        [Parameter] public AddEditDiaryTypeCommand AddEditDiaryTypeModel { get; set; } = new();
+        [CascadingParameter] private MudDialogInstance MudDialog { get; set; }
+        [CascadingParameter] private HubConnection HubConnection { get; set; }
+
+        private FluentValidationValidator _fluentValidationValidator;
+        private bool Validated => _fluentValidationValidator.Validate(options => { options.IncludeAllRuleSets(); });
+
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+
+        private async Task SaveAsync()
+        {
+            var response = await DiaryTypeManager.SaveAsync(AddEditDiaryTypeModel);
+            if (response.Succeeded)
+            {
+                _snackBar.Add(response.Messages[0], Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var message in response.Messages)
+                {
+                    _snackBar.Add(message, Severity.Error);
+                }
+            }
+            await HubConnection.SendAsync(ApplicationConstants.SignalR.SendUpdateDashboard);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadDataAsync();
+            HubConnection = HubConnection.TryInitialize(_navigationManager);
+            if (HubConnection.State == HubConnectionState.Disconnected)
+            {
+                await HubConnection.StartAsync();
+            }
+        }
+
+        private async Task LoadDataAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
